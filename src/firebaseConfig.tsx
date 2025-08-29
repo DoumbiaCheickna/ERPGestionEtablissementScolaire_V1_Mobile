65f40afa670bad11cb266f7fcedf9f1c4576572c
DjibrilMTRAORE <traodjibril.250@gmail.com>
2025-08-29 17:35:13 +0000

    Mise en place des emargements ( Absence Automatique et consulation absences et matières )

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,23 +1,25 @@
 import { collection, doc, DocumentData, getDoc, getDocs, getFirestore, query, QuerySnapshot, updateDoc, where } from "firebase/firestore";
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-import { getAuth } from 'firebase/auth';
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getAuth, initializeAuth } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from "react-native";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAk9ReRsOP3615ysayTcfdEpXyIHv4eCAE",
   authDomain: "gestiondesetablissementsco.firebaseapp.com",
   projectId: "gestiondesetablissementsco",
-  storageBucket: "gestiondesetablissementsco.firebasestorage.app",
+  storageBucket: "gestiondesetablissementsco.appspot.com",
   messagingSenderId: "359588684947",
   appId: "1:359588684947:web:79020fc0da0c3cf2c85966",
   measurementId: "G-T3NGPP7S81"
-};                                                                                                                                                                                                                       const app = initializeApp(firebaseConfig);
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const auth = getAuth(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app)
 
 
 
